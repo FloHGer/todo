22a7519775604e6116abe6222a764a4486f94744
FloHGer <mail@florianhoehle.de>
2021-09-09 21:41:00 +0200

    update

--- main.js
+++ main.js
@@ -1,46 +1,67 @@
+// ON LOAD
 window.onload = renderList();
 
+// SUBMIT NEW DATA
 document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault();
   let formData = new FormData(e.target);
+  // create entry object
   let newEntry = {
-    task: formData.get('task'),
+    name: formData.get('name'),
+    category: formData.get('category'),
     deadline: formData.get('deadline'),
   }
+  // reset inputs
+  document.querySelectorAll('input').forEach(e => e.value = '');
   saveEntry(newEntry);
 })
 
+// SAVE DATA TO LOCALE STORAGE
 function saveEntry(newEntry){
-  console.log(newEntry)
   let tasks = [];
+  // check for available content
   if(localStorage.getItem('entries') != null)
     tasks = JSON.parse(localStorage.getItem('entries'));
 
   tasks.push(newEntry);
+
   localStorage.setItem("entries",  JSON.stringify(tasks));
 
   renderList();
 }
 
+// (RE)RENDER THE LIST
 function renderList(){
   let anchor = document.querySelector('.output');
+  // reset old list
   anchor.innerHTML = '';
+
   let storage = [];
+  // check for available content
   if(localStorage.getItem('entries') != null)
     storage = JSON.parse(localStorage.getItem('entries'));
-
+  // create HTML
   storage.forEach((e, i) => {
     let container = document.createElement('div');
     container.style.border = '1px solid black';
     anchor.appendChild(container);
 
     let name = document.createElement('h3');
-    name.innerText = e.task;
+    name.innerText = e.name;
     container.appendChild(name);
+
+    let category = document.createElement('p');
+    category.innerText = e.category;
+    container.appendChild(category);
 
     let deadline = document.createElement('p');
     deadline.innerText = e.deadline;
     container.appendChild(deadline);
+
+    let markDone = document.createElement('button');
+    markDone.innerText = 'done';
+    markDone.classList.add('doneButtons', `task${i}`);
+    container.appendChild(markDone);
 
     let deleteTask = document.createElement('button');
     deleteTask.innerText = 'delete Task';
@@ -49,22 +70,35 @@ function renderList(){
   })
 }
 
+// DONE BUTTONS
+document.querySelector('.output').addEventListener('click', e => {
+  if(e.target.classList[0] == 'doneButtons'){
+    if(e.target.innerText == 'done') e.target.innerText = 'undone';
+    else e.target.innerText = 'done';
+    e.target.parentElement.classList.toggle('done')
+  }
+    
+});
+
+// CLOSING BUTTONS
 document.querySelector('.output').addEventListener('click', e => {
     if(e.target.classList[0] == 'closingButtons'){
+      // get index number
       let index = e.target.classList[1].slice(4);
       deleteEntry(index);
     }
   });
 
+// DELETE SINGLE ENTRY
 function deleteEntry(index){
-  console.log(index)
   let storage = JSON.parse(localStorage.getItem('entries'));
   storage.splice(index, 1);
   localStorage.setItem("entries", JSON.stringify(storage));
   renderList();
 }
 
-document.querySelector('#reset').addEventListener('click', e => {
+// DELETE ALL ENTRIES
+document.querySelector('#reset').addEventListener('click', () => {
   localStorage.clear();
   renderList();
-})
+});
